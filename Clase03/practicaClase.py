35d13fc068a8eac34dac0d5a5dec536b49307ab8
hayde martinez <haydeml@haydes-personal.local>
2026-05-16 13:28:36 -0600

    Archivos clase 3

---
+++ Clase03/practicaClase.py
@@ -0,0 +1,33 @@
+# Declarando / creando datos atómicos - los más pequeños o los fundamentales
+entero = 1250
+flotantes = 1500.50
+cadena = "Hayde"
+Booleano = True
+
+#Declarando más variables
+resultado = entero + flotantes
+resta = 15 - flotantes
+total = 1 + 2 +  3 + \
+        4 + 5 + 6 
+
+# Más tipos de datos
+lista = [1, 2, "hola", 5+5]
+
+
+# Imprimiendo los valores que acabo de crear
+print(entero)
+print(flotantes)
+print(cadena)
+print(Booleano)
+print(resultado)
+print(resta)
+print(total)
+
+# Creando una función
+
+def suma(): # <- Para crear una funciòn siempre uso la palabra reservada def despues un espacio, despuès el nombre de la funciòn, después parentesis abierto y cerrado y finalmente un ":"
+    print(2+2)
+
+# Llamar mi función
+
+suma()
